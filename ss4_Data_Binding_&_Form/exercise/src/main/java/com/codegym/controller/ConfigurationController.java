d7bced7d72124e8282cfbf973fb6c8e0995d1f05
Your Name <you@example.com>
2022-12-18 10:52:36 +0700

    nap bai tap ss4

--- ss4_Data_Binding_&_Form/exercise/src/main/java/com/codegym/controller/ConfigurationController.java
+++ ss4_Data_Binding_&_Form/exercise/src/main/java/com/codegym/controller/ConfigurationController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("")
 public class ConfigurationController {
     @Autowired
     private IElectronicMailService electronicMailService;
